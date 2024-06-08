b5a337a1f2b91d9b58f9ce6909b6d0a501f141ab
Aditya Oberai <adityaoberai1@gmail.com>
2024-06-08 06:44:12 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { user } from '$lib/user';
+	import InitHeading from '../components/InitHeading.svelte';
+	import NavBar from '../components/NavBar.svelte';
+
+	function login() {
+		user.login();
+	}
+</script>
+
+<NavBar />
+<section class="u-flex-vertical">
+	<div class="container u-flex-vertical">
+		<InitHeading heading="Giveaway" />
+
+		<img src="/giveaway.png" alt="Plain black T-shirt with the text Init and a keyboard branded with the Appwrite logo on the Escape key" />
+
+		<p class="heading-level-6 u-margin-32">
+			Login with Discord and get a chance to win some amazing gifts!
+		</p>
+
+		<button class="button is-big u-margin-32" on:click={login}>
+			<span class="icon-discord"></span>
+			Sign in with Discord
+		</button>
+	</div>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		z-index: 0;
+		position: relative;
+		top: 0;
+		width: fit-content;
+		height: min-content;
+		margin: auto;
+		text-align: center;
+	}
+
+	img {
+		border-radius: var(--border-radius-medium);
+	}
+
+	@media (max-width: 768px) {
+		img {
+			max-width: 90%;
+		}
+	}
+
+	@media (min-width: 768px) {
+		img {
+			max-width: 75%;
+		}
+	}
+</style>
---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { user } from '$lib/user';
+	import { db } from '$lib/database';
+	import { onMount } from 'svelte';
+	import NavBar from '../../components/NavBar.svelte';
+
+	let userId = '';
+
+	async function getUserId() {
+		var currentUser = await user.get();
+		await db.add(currentUser.name, currentUser.email);
+		userId = currentUser.name;
+	}
+
+	onMount(() => {
+		getUserId();
+	});
+</script>
+
+<NavBar />
+
+<div class="container u-flex-vertical">
+	<h1 class="heading-level-1 u-margin-32 u-normal">Success!</h1>
+	<p class="body-text-1 u-margin-32 u-normal">Thanks for participating in the giveaway, {userId}</p>
+</div>
