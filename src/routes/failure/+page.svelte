f728096225b886566107585455fc21b4223f9aa3
adityaoberai <adityaoberai1@gmail.com>
2024-06-10 21:24:28 +0530

    minor updates in success failure pages

--- src/routes/failure/+page.svelte
+++ src/routes/failure/+page.svelte
@@ -6,6 +6,6 @@
 
 <div class="container u-flex-vertical">
 	<h1 class="heading-level-1 u-normal u-padding-32">
-		Could not login with Discord, please try again!
+		Could not login with GitHub, please try again!
 	</h1>
 </div>
